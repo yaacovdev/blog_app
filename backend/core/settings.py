71f50634df7d0318c453bfdad620f10a62195745
yaacovdev <yaacov@purpl-ride.com>
2024-06-17 17:32:04 +0300

    add: blog app

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+INSTALLED_APPS += [
+    'rest_framework',
+    'blog',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
