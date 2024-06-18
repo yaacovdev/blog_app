0737a8b1abb2a54496cd50005d3a4c74768ef4c8
yaacovdev <yaacov@purpl-ride.com>
2024-06-18 09:01:54 +0300

    chore: Add error middleware for handling messaging system errors

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS += [
 ]
 
 MIDDLEWARE = [
+    "blog.error_middleware.ErrorMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
