9024fd33db3838f1c742857491d482c438c18ff1
yaacovdev <yaacov@purpl-ride.com>
2024-06-20 19:49:46 +0300

    feat: Register Post and Comment models in admin site

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -20,6 +20,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self) -> str:
+        return self.title
+
 
 class Comment(models.Model):
     """
